39b07ff604172c871f0b2788088850dc1e8557b5
Kartikey Dubey <63945298+dubeykartikey@users.noreply.github.com>
2022-10-11 18:15:37 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,58 @@
+from distutils.log import ERROR
+from email import message
+import enum
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents2.json').read())
+words=pickle.load(open('words.pkl','rb'))
+classes=pickle.load(open('classes.pkl','rb'))
+model= load_model('chatbotmodel.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words= clean_up_sentence(sentence)
+    bag=[0]*len(words)
+    for w in sentence_words:
+        for i,word in enumerate(words):
+            if word==w:
+                bag[i]=1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    result =[[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+    result.sort(key=lambda x:x[1],reverse=True)
+    return_list =[]
+    for r in result:
+        return_list.append({'intent':classes[r[0]],'probability':str(r[1])})
+    return return_list
+def get_response(intents_list,intents_json):
+    tag=intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag']==tag:
+            return random.choice(i['responses'])
+print("Who can i help you!")
+while True:
+    message = input(" You: ")
+    if message == "thank you":
+        print("Your Welcome")
+        break
+    ints=predict_class(message)
+    res=get_response(ints,intents)
+    print("DocBot:",res)
